d14faa12d4a42d11c0b9a7e4b314217296a985e4
Kiraraah.Leislane <Srtaleislane@gmail.com>
2023-11-29 23:20:22 -0300

    filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -36,6 +36,26 @@ app.post('/completar', (requisicao, resposta) => {
     })
 })
 
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    console.log(id)
+
+    const sql = `
+        UPDATE tarefas 
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar',(requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -62,8 +82,6 @@ app.get('/', (requisicao,resposta) => {
             return console.log(erro)
         }
 
-        
-
         const tarefas = dados.map((dado) => {
             return {
                 id: dado.id,
@@ -72,7 +90,13 @@ app.get('/', (requisicao,resposta) => {
             }
         })
 
-        resposta.render('home', {tarefas})
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', { tarefas, quantidadeTarefasAtivas })
     })
 })
 
